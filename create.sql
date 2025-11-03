0ab803cdefbd8a6d54d074f368df8a0e793d25f0
JoaoPauloQC <joaopauloqueirozcosta@gmail.com>
2025-11-03 16:16:49 -0300

    refactor: mais separacao

--- create.sql
+++ create.sql
@@ -20,11 +20,3 @@ VALUES ('04496332780', 'João da Silva', '1969-11-25',
 Soares', '1990-03-05', 'Rua Palmeias Novas', '88048917', 'Copacabana', 'Rio de Janeiro', 'RJ')
 ;
 
-INSERT INTO cliente (cpf,nome,data_ultima_compra,endereco,cep,bairro,cidade,uf)
-VALUES ( '07788991001', 'Manuela Teixeira Justos',
-'1989-11-05', 'Praça Alberto Lion', '01515000', 'Cambuci', 'São Paulo', 'SP' ),( '05554031942', 'Maria Eduarda
-Teles', '1988-07-01', 'Rua Floriano Peixoto', '35930500', 'Jardim Parque Morada do Sol', 'Montes Claros',
-'MG'),( '46390317903', 'Benedito Silva Santos', '1999-08-17', 'Alameda Princesa Izabel', '80430128',
-'Centro', 'Curitiba', 'PR'
-);
-ALTER TABLE cliente add column idade smallint;
