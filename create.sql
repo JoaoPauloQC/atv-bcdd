2848608e8f77a2ac275aa1f08ef8161630cf64f9
JoaoPauloQC <joaopauloqueirozcosta@gmail.com>
2025-11-03 16:18:42 -0300

    refactor: melhor divisao

--- create.sql
+++ create.sql
@@ -11,12 +11,4 @@ cpf varchar(100) PRIMARY KEY,
     uf varchar(50)
 );
 
-ALTER TABLE cliente ADD COLUMN data_ultima_compra date;
-
-INSERT INTO cliente (cpf,nome,data_ultima_compra,endereco,cep,bairro,cidade,uf)
-VALUES ('04496332780', 'João da Silva', '1969-11-25',
-'Rua Antônio Numes', '88045963', 'Palmeiras', 'Londrina', 'PR' ) , ( '05485031490', 'Ana Regina Fagundes',
-'1986-09-21', 'Rua Palmeias Novas', '88078923', 'Leblon', 'Rio de Janeiro', 'RJ' ) ,('03350314905', 'Fernando
-Soares', '1990-03-05', 'Rua Palmeias Novas', '88048917', 'Copacabana', 'Rio de Janeiro', 'RJ')
-;
 
